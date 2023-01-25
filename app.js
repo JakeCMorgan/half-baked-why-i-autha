016474dd37a8481c6501cf26028da74b20172d21
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-25 12:40:58 -0800

    initial commit, grabbed DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const signInForm = document.getElementById('sign-in');
+const singInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
